41a89a0454cbb6692e34efd37d3b062ccd2ff692
Becky Pier <becky.pier.@gmail.com>
2013-04-15 11:05:34 -0700

    It turns out recursion wasn't the fastest way to solve that problem. Go figure.

--- stripper.py
+++ stripper.py
@@ -9,36 +9,37 @@ MAX_REQUESTABLE_TWEETS = 200
 def consume_timeline():
 	# first request returns maximum allowable number of tweets
 	# include_rts must be true in order to return the full number of results specified by count
-	status = AUTHENTICATED_API.GetUserTimeline(screen_name=USER_NAME, count=100, include_rts=True)
+	status = AUTHENTICATED_API.GetUserTimeline(screen_name=USER_NAME, count=200, include_rts=True)
 	max_id_returned = status[-1].id
 	print "max_id = ", max_id_returned
 	print "text: ", status[-1].text
 	print "length:", len(status)
 
 	# Does it make my code run more slowly to define a function inside a function? Should this live somewhere else?
-	def get_page(max_id_returned):
-		new_status = api.GetUserTimeline(screen_name=USER_NAME, count=MAX_REQUESTABLE_TWEETS, include_rts=True, 
-			max_id = max_id_returned)
+	def get_page(max_id_returned, status_length):
+		while status_length != 0:
+			new_status = AUTHENTICATED_API.GetUserTimeline(screen_name=USER_NAME, count=MAX_REQUESTABLE_TWEETS, 
+				include_rts=True, max_id = max_id_returned)
 
-		# new_status[0] is a duplicate (equivalent to last tweet from previous request)
-		# so we throw it out
-		new_status.pop(0)
+			# new_status[0] is a duplicate (equivalent to last tweet from previous request)
+			# so we throw it out
+			new_status.pop(0)
 
-		status.extend(new_status)
-		max_id_returned_2 = new_status[-1].id
-		print "text: ", new_status[-1].text
-		print "tweet_id", new_status[-1].id
-		print "len new_status", len(new_status)
-		print "len status", len(status)
-		# with len(new_status) < 196, it returns 2882 results.
-		# with len(new_status) >= 196, I get 1687 results
-		# I am confuse
-		if len(new_status) < 196:
-			return len(new_status)
-		else:
-			get_page(max_id_returned_2)
+			status_length = len(new_status)
+			print "status_length", status_length
+			last_status_index = status_length - 1
+			print "last_status_index", last_status_index
 
-	#get_page(max_id_returned)
+			if status_length == 0:
+				return
+
+			status.extend(new_status)
+			max_id_returned = new_status[last_status_index].id
+			print "text: ", new_status[last_status_index].text
+			print "tweet_id", new_status[last_status_index].id
+			print "len status", len(status)
+
+	get_page(max_id_returned, len(status))
 
 	# spot checking for duplicate tweets
 	# for item in status:
